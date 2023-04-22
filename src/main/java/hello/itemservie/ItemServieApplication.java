654379196bcab8b5ccd834f9de81cb229f265816
LeeTangle <adszzz11@naver.com>
2023-04-22 18:58:21 +0900

    initial commit

---
+++ src/main/java/hello/itemservie/ItemServieApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/itemservie/ItemServieApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemservie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
